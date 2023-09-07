e52b4f5af00f8ab73b77361afd83edae3176e22e
Андрей Карпов <karpov.andrei.000@mail.ru>
2023-09-07 17:45:58 +0500

    Task4

---
+++ Tyuiu.KarpovAA.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KarpovAA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KarpovAA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b6293bed-8e8d-4578-b56c-cf5d2f105192")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
