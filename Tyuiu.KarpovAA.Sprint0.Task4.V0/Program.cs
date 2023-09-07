535fd3e2ed7a0fc6ffdb0c8c0ec3b6ad64a7a0a0
Андрей Карпов <karpov.andrei.000@mail.ru>
2023-09-07 17:48:13 +0500

    Task4

--- Tyuiu.KarpovAA.Sprint0.Task4.V0/Program.cs
+++ Tyuiu.KarpovAA.Sprint0.Task4.V0/Program.cs
@@ -10,6 +10,7 @@ namespace Tyuiu.KarpovAA.Sprint0.Task4.V0
     {
         static void Main(string[] args)
         {
+            //линейная
             Console.WriteLine(DataService.Addition(1, 5));
             Console.WriteLine(DataService.Subtraction(15, 5));
             Console.WriteLine(DataService.Multiplication(10, 10));
