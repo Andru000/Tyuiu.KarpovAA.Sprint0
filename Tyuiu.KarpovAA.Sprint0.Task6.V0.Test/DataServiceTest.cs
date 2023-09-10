e41e923daf6f17ea5e8b7b2b70d611d32751d551
Андрей Карпов <karpov.andrei.000@mail.ru>
2023-09-10 16:06:06 +0500

    Task6

---
+++ Tyuiu.KarpovAA.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KarpovAA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KarpovAA.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckSubstractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubstractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+        [TestMethod]
+        public void CheckMultitionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultitionArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+
+    }
+}
